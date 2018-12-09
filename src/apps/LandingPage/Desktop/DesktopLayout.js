6a52dea56fd087183af2b920e50a4c80300fb86f
Amir Reza <arg7971@gmail.com>
2018-12-10 03:09:38 +0330

    test some components

--- src/apps/LandingPage/Desktop/DesktopLayout.js
+++ src/apps/LandingPage/Desktop/DesktopLayout.js
@@ -1,25 +1,57 @@
 import React, { Component } from "react";
 import ParticleBackground from "../../../components/ParticleBackground/ParticleBackground";
 import FirstHexagon from "../../../components/Hexagons/FirstHexagon/FirstHexagon";
+import ThirdHexagon from "../../../components/Hexagons/ThirdHexagon/ThirdHexagon";
+import SecondHexagon from "../../../components/Hexagons/SecondHexagon/SecondHexagon";
+import FourthHexagon from "../../../components/Hexagons/FourthHexagon/FourthHexagon";
 
 class DesktopLayout extends Component {
   render() {
     return (
       <div>
         <ParticleBackground />
+
         <div
           style={{
             position: "absolute",
             right: 100,
-            top: 50,
-            borderRadius: "50%",
-            backgroundColor: "rgba(0,0,0,0.35)",
-            padding: 10,
-            paddingTop: 25,
-            paddingRight: 30
+            top: 0
           }}
         >
-          <FirstHexagon />
+          <FirstHexagon
+            width={300}
+            height={300}
+          />
+        </div>
+
+        <div
+          style={{
+            position: "absolute",
+            right: 400,
+            top: 0
+          }}
+        >
+          <SecondHexagon />
+        </div>
+
+        <div
+          style={{
+            position: "absolute",
+            right: 750,
+            top: 0
+          }}
+        >
+          <ThirdHexagon />
+        </div>
+
+        <div
+          style={{
+            position: "absolute",
+            right: 1050,
+            top: 0
+          }}
+        >
+          <FourthHexagon />
         </div>
       </div>
     );
