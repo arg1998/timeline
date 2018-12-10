07f344046255a5ab781cee85e1aca890c1bf5209
Amir Reza <arg7971@gmail.com>
2018-12-10 16:44:54 +0330

    temp layout .::JUST FOR TESTING::.

--- src/apps/LandingPage/Desktop/DesktopLayout.js
+++ src/apps/LandingPage/Desktop/DesktopLayout.js
@@ -4,54 +4,81 @@ import FirstHexagon from "../../../components/Hexagons/FirstHexagon/FirstHexagon
 import ThirdHexagon from "../../../components/Hexagons/ThirdHexagon/ThirdHexagon";
 import SecondHexagon from "../../../components/Hexagons/SecondHexagon/SecondHexagon";
 import FourthHexagon from "../../../components/Hexagons/FourthHexagon/FourthHexagon";
+import AbstractHexagon from "../../../components/AbstractHexagon/AbstractHexagon";
 
 class DesktopLayout extends Component {
   render() {
     return (
       <div>
         <ParticleBackground />
-
         <div
           style={{
-            position: "absolute",
-            right: 100,
-            top: 0
+            position: "fixed",
+            width: "100%",
+            height: 64,
+            top: 0,
+            left: 0,
+            backgroundColor: "rgba(100,50,200,0.4)",
+            borderBottom: "2px rgba(100,50,100,0.9) solid"
           }}
-        >
-          <FirstHexagon
-            width={300}
-            height={300}
-          />
-        </div>
+        />
 
-        <div
-          style={{
-            position: "absolute",
-            right: 400,
-            top: 0
-          }}
-        >
-          <SecondHexagon />
-        </div>
+        <div style={{ width: "100%", marginTop: 64 , position:'relative', paddingTop: 20}}>
+          <div
+            style={{
+              width: "15vw",
+              height: "15vw",
+              position: "absolute",
+              left: "75vw"
+            }}
+          >
+            <FirstHexagon width="100%" height="100%" />
+          </div>
 
-        <div
-          style={{
-            position: "absolute",
-            right: 750,
-            top: 0
-          }}
-        >
-          <ThirdHexagon />
-        </div>
+          <div
+            style={{
+              width: "20vw",
+              height: "15vw",
+              position: "absolute",
+              bottom: "5vh",
+              left: "75vw"
+            }}
+          >
+            <SecondHexagon width="100%" height="100%" />
+          </div>
 
-        <div
-          style={{
-            position: "absolute",
-            right: 1050,
-            top: 0
-          }}
-        >
-          <FourthHexagon />
+          <div
+            style={{
+              width: "15vw",
+              height: "15vw",
+              position: "absolute",
+              bottom: "5vh",
+              left: "10vw"
+            }}
+          >
+            <ThirdHexagon width="100%" height="100%" />
+          </div>
+
+          <div
+            style={{
+              width: "15vw",
+              height: "15vw",
+              position: "absolute",
+              left: "10vw"
+            }}
+          >
+            <FourthHexagon width="100%" height="100%" />
+          </div>
+
+          <div
+            style={{
+              width: "85vh",
+              height: "85vh",
+              margin: "0 auto"
+            }}
+          >
+            <AbstractHexagon width="100%" height="100%" />
+          </div>
         </div>
       </div>
     );
