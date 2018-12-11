d9df0909fb3f6e61af11f895db4194bb8af04957
shima <shima.shlan1997@gmail.com>
2018-12-11 23:51:48 +0330

    first Layout design

--- src/apps/LandingPage/Desktop/DesktopLayout.js
+++ src/apps/LandingPage/Desktop/DesktopLayout.js
@@ -1,83 +1,23 @@
 import React, { Component } from "react";
 import ParticleBackground from "../../../components/ParticleBackground/ParticleBackground";
-import FirstHexagon from "../../../components/Hexagons/FirstHexagon/FirstHexagon";
-import ThirdHexagon from "../../../components/Hexagons/ThirdHexagon/ThirdHexagon";
-import SecondHexagon from "../../../components/Hexagons/SecondHexagon/SecondHexagon";
-import FourthHexagon from "../../../components/Hexagons/FourthHexagon/FourthHexagon";
-import AbstractHexagon from "../../../components/AbstractHexagon/AbstractHexagon";
+import DesktopStyles from "./DesktopLayout.style";
 
 class DesktopLayout extends Component {
   render() {
+    const { classes, children } = this.props;
     return (
-      <div>
+      <div style={{ height: "100%" }}>
         <ParticleBackground />
-        <div
-          style={{
-            position: "fixed",
-            width: "100%",
-            height: 64,
-            top: 0,
-            left: 0,
-            backgroundColor: "rgba(100,50,200,0.4)",
-            borderBottom: "2px rgba(100,50,100,0.9) solid"
-          }}
-        />
-
-        <div style={{ width: "100%", marginTop: 64 , position:'relative', paddingTop: 20}}>
-          <div
-            style={{
-              width: "15vw",
-              height: "15vw",
-              position: "absolute",
-              left: "75vw"
-            }}
-          >
-            <FirstHexagon width="100%" height="100%" />
+        <div className={classes.container}>
+          <div className={classes.left_side}>
+            <div className={classes.first_hex} />
+            <div className={classes.second_hex} />
           </div>
+          <div className={classes.main_hex} />
 
-          <div
-            style={{
-              width: "20vw",
-              height: "15vw",
-              position: "absolute",
-              bottom: "5vh",
-              left: "75vw"
-            }}
-          >
-            <SecondHexagon width="100%" height="100%" />
-          </div>
-
-          <div
-            style={{
-              width: "15vw",
-              height: "15vw",
-              position: "absolute",
-              bottom: "5vh",
-              left: "10vw"
-            }}
-          >
-            <ThirdHexagon width="100%" height="100%" />
-          </div>
-
-          <div
-            style={{
-              width: "15vw",
-              height: "15vw",
-              position: "absolute",
-              left: "10vw"
-            }}
-          >
-            <FourthHexagon width="100%" height="100%" />
-          </div>
-
-          <div
-            style={{
-              width: "50vw",
-              height: "85vh",
-              margin: "0 auto"
-            }}
-          >
-            <AbstractHexagon width="100%" height="100%" />
+          <div className={classes.right_side}>
+            <div className={classes.three_hex} />
+            <div className={classes.four_hex} />
           </div>
         </div>
       </div>
@@ -85,4 +25,4 @@ class DesktopLayout extends Component {
   }
 }
 
-export default DesktopLayout;
+export default DesktopStyles(DesktopLayout);
