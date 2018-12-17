7dfc8f8ad84d96fb4c8186996c2231c3d7c9b46c
Amir Reza <arg7971@gmail.com>
2018-12-18 00:27:18 +0330

    created a demo and a firefox fix for not supporting vh and vw on <svg />

--- src/apps/LandingPage/Desktop/DesktopLayout.js
+++ src/apps/LandingPage/Desktop/DesktopLayout.js
@@ -8,35 +8,77 @@ import ThirdHexagon from "../../../components/Hexagons/ThirdHexagon/ThirdHexagon
 import SecondHexagon from "../../../components/Hexagons/SecondHexagon/SecondHexagon";
 import AbstractHexagon from "../../../components/AbstractHexagon/AbstractHexagon";
 
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class DesktopLayout extends Component {
+  state = { activeIndex: -1 };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState, newProps) => ({
+        activeIndex: (prevState.activeIndex + 1) % 4
+      }));
+    }, 3000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
   render() {
     const { classes, aspectRatio } = this.props;
+    const { activeIndex } = this.state;
     if (aspectRatio < 1.5) {
       return (
         <p>
-          Aspect Ratio is not supported, current AR is {aspectRatio.toFixed(2)}{" "}
+          Aspect Ratio is not supported, current AR is {aspectRatio.toFixed(2)}
           but to see the website you must provide a screen with bigger AR than
           1.5
         </p>
       );
     }
+
     return (
       <div className={classes.root_container}>
         <ParticleBackground count={100} />
 
         <div className={classes.rootContainer}>
           <div className={classes.sideBar}>
-            <ThirdHexagon width="25vh" height="25vh" />
-            <FourthHexagon width="25vh" height="25vh" />
+            <div style={{ width: "25vh", height: "25vh" }}>
+              <FourthHexagon
+                width="100%"
+                height="100%"
+                active={activeIndex == 3}
+              />
+            </div>
+            <div style={{ width: "25vh", height: "25vh" }}>
+              <ThirdHexagon
+                width="100%"
+                height="100%"
+                active={activeIndex == 2}
+              />
+            </div>
           </div>
 
-          <div>
-            <AbstractHexagon width="100vh" height="95vh" />
+          <div style={{ width: "100vh", height: "95vh" }}>
+            <AbstractHexagon width="100%" height="100%" />
           </div>
 
           <div className={classes.sideBar}>
-            <FirstHexagon width="25vh" height="25vh" />
-            <SecondHexagon width="25vh" height="25vh" />
+            <div style={{ width: "25vh", height: "25vh" }}>
+              <FirstHexagon
+                width="100%"
+                height="100%"
+                active={activeIndex == 0}
+              />
+            </div>
+            <div style={{ width: "25vh", height: "25vh" }}>
+              <SecondHexagon
+                width="100%"
+                height="100%"
+                active={activeIndex == 1}
+              />
+            </div>
           </div>
         </div>
       </div>
