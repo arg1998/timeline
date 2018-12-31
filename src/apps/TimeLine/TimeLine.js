5d293c29bf8033ebe02ce666d8520dccb2cddc8a
Amir Reza <arg7971@gmail.com>
2018-12-31 19:03:45 +0330

    created Event component

--- src/apps/TimeLine/TimeLine.js
+++ src/apps/TimeLine/TimeLine.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import TimeLineLayout from "../../containers/TimeLineLayout/TimeLineLayout";
 import withSizes from "react-sizes";
-//import TimeLineStyle from "./TimeLine.style";
+import TimeLineStyle from "./TimeLine.style";
 import bp from "../../utils/BreakPoints";
-
+import TimeLineEvent from "../../components/TimeLineEvent/TimeLineEvent";
+import fakeData from "../../res/FakeJsonData";
 class TimeLine extends Component {
   render() {
     const { isDesktop } = this.props;
 
-    if (isDesktop) {
-      return (
-        <div>
-          <TimeLineLayout />
-        </div>
-      );
-    } else {
-      return <p>fuck you</p>;
-    }
+    return (
+      <div>
+        <TimeLineLayout isDesktop={isDesktop}>
+          {fakeData.map((e, i) => (
+            <TimeLineEvent
+              key={e.key}
+              isDesktop={isDesktop}
+              index={i}
+              eventData={e}
+            />
+          ))}
+        </TimeLineLayout>
+      </div>
+    );
   }
 }
 
@@ -25,4 +31,4 @@ const mapSizesToProps = ({ width, height }) => ({
   screen: { w: width, h: height }
 });
 
-export default withSizes(mapSizesToProps)(TimeLine);
+export default withSizes(mapSizesToProps)(TimeLineStyle(TimeLine));
---
+++ src/components/TimeLineEvent/Event/Event.style.js
@@ -0,0 +1,56 @@
+import injectSheet from "react-jss";
+
+const styles = {
+  sideContainer: ({ isDesktop }) => ({
+    height: "100%",
+    boxSizing: "border-box",
+    zIndex: 1,
+    width: isDesktop ? "50%" : "100%",
+    display: "flex",
+    flexDirection: "row",
+    position: "relative"
+  }),
+  horizontalBar: ({ horizontalBarSize }) => ({
+    width: "100%",
+    height: horizontalBarSize,
+    backgroundColor: "#fff",
+    position: "absolute",
+    bottom: -(horizontalBarSize / 2)
+  }),
+  gap: ({ isDesktop }) => ({
+    flexGrow: isDesktop ? 3 : 10,
+    minWidth: isDesktop ? 150 : 60,
+    boxSizing: "border-box"
+  }),
+  contentContainer: {
+    flexGrow: 10,
+    boxSizing: "border-box",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-end"
+  },
+  eventBox: ({ isDesktop }) => ({
+    width: "100%",
+    borderRadius: "5px 5px 0 0 ",
+    minHeight: isDesktop?  180 : 120,
+    maxHeight: 350,
+    overflow: "hidden",
+    padding: 10,
+    backgroundColor: "#254"
+  }),
+  eventTitle: {
+    padding: 0,
+    margin: 0,
+    marginLeft: 0,
+    color: "#fff",
+    fontFamily: "Scope One"
+  },
+  eventImage: ({ isDesktop }) => ({
+    width: isDesktop ? 160 : 100,
+    height: isDesktop ? 140 : 70,
+    float: "left",
+    marginRight: 10
+  })
+};
+
+export default component => injectSheet(styles)(component);
