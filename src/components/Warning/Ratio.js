f5d52bb07d1b294f59fe03edba565906f17dd653
shima <shima.shlan1997@gmail.com>
2019-01-02 21:59:24 +0330

    Shakila Fixed Component For Warning

---
+++ src/components/Warning/Ratio.js
@@ -0,0 +1,20 @@
+import React from "react";
+import warning from "../../res/images/warning.svg";
+
+function Ratio() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightblue"
+      }}
+    >
+      <img src={warning} style={{ width: 300, height: 400 }} />
+    </div>
+  );
+}
+export default Ratio;
