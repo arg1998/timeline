55929c5f9c583664f8ba142c1e64cc9690eb0a67
Amir Reza <arg7971@gmail.com>
2019-01-03 00:46:07 +0330

    fixed some bugs on <FullInformation />

--- src/components/FullInformation/FullInformation.js
+++ src/components/FullInformation/FullInformation.js
@@ -2,22 +2,25 @@ import React from "react";
 import FullInformationStyles from "./FullInformation.style";
 
 const DATE_OPTIONS = {
-  weekday: "long",
   year: "numeric",
   month: "long",
   day: "numeric"
 };
 
 function FullInformation(props) {
-  const { classes, title, description, date, imageUrl, onClose } = props;
+  const { classes, eventData, onClose } = props;
+  if (!eventData) {
+    return null;
+  }
+  const { title, description, date, imageUrl } = eventData;
   return (
-    <div className={classes.rootContainer}>
-      <div className={classes.close} onClick={onClose || (() => null)}>
+    <div className={classes.rootContainer} onClick={onClose || (() => null)}>
+      <div className={classes.close}>
         <p>X</p>
       </div>
       <h1 className={classes.title}>{title}</h1>
       <p className={classes.date}>
-        {date.toLocaleDateString("en-US", DATE_OPTIONS)}
+        {new Date(date).toLocaleDateString("en-US", DATE_OPTIONS)}
       </p>
       <img alt="" src={imageUrl} className={classes.image} />
       <p className={classes.description}>{description}</p>
